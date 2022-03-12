6741361c3a0e1b9ffb68bc7b315510fa632f13a2
Colinmyers1 <colinmyers96@gmail.com>
2022-03-12 14:51:39 -0500

    Homework 6 Vanquished

--- Homework 06/starter_code/js/trulia.js
+++ Homework 06/starter_code/js/trulia.js
@@ -1,30 +1,29 @@
-
+let navToggle = document.querySelector(".trulia-nav-toggle");
+let navItems = document.querySelectorAll(".trulia-nav-mobilehide");
 
 let truliaCards = document.getElementsByClassName("trulia-grid-item");
 
-
-
-document.addEventListener('DOMContentLoaded', function(event) {
-
-// Toggle the navigation
-
-
-
-
-
-
-// Loop through all the cards
-    
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Toggle the navigation
+  // Loop through all the cards
   // Add a click listener on each card
-      
-      // Remove the featured class
+  // Remove the featured class
+  // Add the featured class on the one clicked on
+  navToggle.addEventListener("click", function (e) {
+    e.preventDefault();
 
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].classList.toggle("trulia-nav-mobilehide");
+    }
+  });
 
-      // Add the featured class on the one clicked on
+  for (let i = 0; i < truliaCards.length; i++) {
+    truliaCards[i].addEventListener("click", function () {
+      for (let j = 0; j < truliaCards.length; j++) {
+        truliaCards[j].classList.remove("trulia-featured-grid-item");
+      }
 
-
-
-  });  
-
-
-
+      this.classList.add("trulia-featured-grid-item");
+    });
+  }
+});
